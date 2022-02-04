3baf47d8bdd54adcc7215e375df538a4af4c09f9
maarongross <97714828+maarongross@users.noreply.github.com>
2022-02-04 15:14:16 -0800

    made the test a test and its bad

--- JavaTutorialTest.java
+++ JavaTutorialTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class JavaTutorialTest {
+    @Test
     public void multTest() {
         assertEquals(6, JavaTutorial.mult(2, 2));
     }
